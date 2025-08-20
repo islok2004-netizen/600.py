16dac722147264c0d943fef9344baee820f63b6c
PoisonGURU <islok2004@gmail.com>
2025-08-20 11:14:15 +0530

    new changes on this repo

--- 2.py
+++ 2.py
@@ -3,6 +3,6 @@ def append_my_list(value,my_list=[]):
     my_list.append(value)
     return my_list
 
-print(append_my_list(1))
-print(append_my_list(2))
 print(append_my_list(3))
+print(append_my_list(4))
+print(append_my_list(5))
